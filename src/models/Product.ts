2a0f63c284bf96c0a91d382c01e09cb2fa76e53a
Your Name <mugeta88@gmail.com>
2025-11-21 16:10:02 -0500

    Initial project setup

---
+++ src/models/Product.ts
@@ -0,0 +1,16 @@
+class Product {
+    sku: string;
+    name: string;
+    price: string;
+
+
+    constructor(sku: string, name: string, price: string){
+        this.sku = sku;
+        this.name = name;
+        this.price = price;
+    }
+
+    displayDetails(): string {
+        return ``
+    }
+}
